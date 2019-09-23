55ef99969d1f01fcf20182478f7b372fc5542909
Martin Feckie <mfeckie@gmail.com>
2019-09-23 21:12:05 +0800

    Add first simple test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,18 +1,48 @@
+import * as path from 'path';
 import * as assert from 'assert';
 import { before } from 'mocha';
+import * as fs from 'fs';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
+import { fstat } from 'fs-extra';
 // import * as myExtension from '../extension';
+
+const fixturePath = path.join(__dirname, 'index', 'test-fixtures');
+
+function getFixtures(filename: string) {
+	const infilePath = path.join(fixturePath, filename + '.in.hbs');
+	const outfilePath = path.join(fixturePath, filename + '.out.hbs');
+
+	return {
+		infilePath,
+		infile: fs.readFileSync(infilePath),
+		outfile: fs.readFileSync(outfilePath)
+	};
+}
+
+async function loadFixture(fixture: any) {
+	const uri = vscode.Uri.file(fixture.infilePath);
+	const document = await vscode.workspace.openTextDocument(uri);
+	await vscode.window.showTextDocument(document);	
+	return document;
+}
+
 
 suite('Extension Test Suite', () => {
 	before(() => {
 		vscode.window.showInformationMessage('Start all tests.');
 	});
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+	test('Simple format', async () => {
+		const fixtures = getFixtures('simple');
+  	const document = await loadFixture(fixtures);
+
+		assert.equal(document.getText(), fixtures.infile);
+
+		vscode.commands.executeCommand('editor.action.formatDocument');
+
+		assert.equal(document.getText(), fixtures.outfile);
 	});
 });
