c2fae5243a0ffa96b7515797394bd3f11fe9d2a1
Martin Feckie <mfeckie@gmail.com>
2019-09-24 05:50:29 +0800

    Update extension test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -9,40 +9,48 @@ import * as vscode from 'vscode';
 import { fstat } from 'fs-extra';
 // import * as myExtension from '../extension';
 
-const fixturePath = path.join(__dirname, 'index', 'test-fixtures');
+const fixturePath = path.join(__dirname, 'test-fixtures');
 
 function getFixtures(filename: string) {
-	const infilePath = path.join(fixturePath, filename + '.in.hbs');
-	const outfilePath = path.join(fixturePath, filename + '.out.hbs');
+  const infilePath = path.join(fixturePath, filename + '.in.hbs');
+  const outfilePath = path.join(fixturePath, filename + '.out.hbs');
 
-	return {
-		infilePath,
-		infile: fs.readFileSync(infilePath),
-		outfile: fs.readFileSync(outfilePath)
-	};
+  return {
+    infilePath,
+    infile: fs.readFileSync(infilePath, { encoding: 'utf8' }),
+    outfile: fs.readFileSync(outfilePath, { encoding: 'utf8' })
+  };
 }
 
 async function loadFixture(fixture: any) {
-	const uri = vscode.Uri.file(fixture.infilePath);
-	const document = await vscode.workspace.openTextDocument(uri);
-	await vscode.window.showTextDocument(document);	
-	return document;
+
+  const uri = vscode.Uri.file(fixture.infilePath);
+  const document = await vscode.workspace.openTextDocument(uri);
+  await vscode.window.showTextDocument(document);
+  return document;
 }
 
-
 suite('Extension Test Suite', () => {
-	before(() => {
-		vscode.window.showInformationMessage('Start all tests.');
-	});
+  test('Simple format', async () => {
+    const fixtures = getFixtures('simple');
+    const document = await loadFixture(fixtures);
 
-	test('Simple format', async () => {
-		const fixtures = getFixtures('simple');
-  	const document = await loadFixture(fixtures);
+    assert.equal(document.getText(), fixtures.infile);
 
-		assert.equal(document.getText(), fixtures.infile);
+    await vscode.commands.executeCommand('editor.action.formatDocument');
 
-		vscode.commands.executeCommand('editor.action.formatDocument');
+    assert.equal(document.getText(), fixtures.outfile);
+  });
 
-		assert.equal(document.getText(), fixtures.outfile);
-	});
+  test('Complex format', async () => {
+    const fixtures = getFixtures('complex');
+    const document = await loadFixture(fixtures);
+
+    assert.equal(document.getText(), fixtures.infile);
+
+    await vscode.commands.executeCommand('editor.action.formatDocument');
+
+    assert.equal(document.getText(), fixtures.outfile);
+    assert.notEqual(document.getText(), fixtures.infile);
+  });
 });
